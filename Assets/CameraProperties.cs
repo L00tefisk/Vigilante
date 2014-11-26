44ccf4b465b64cb9bbcec112b26b7b21955dc11e
Eivind Hyldmo <Eivind.93@hotmail.com>
2014-11-26 21:57:42 +0100

    Initial Commit

---
+++ Assets/CameraProperties.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class CameraProperties : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Camera>().transparencySortMode = TransparencySortMode.Orthographic;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
