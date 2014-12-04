9ddad3882959f78c1b98abe45b17d3f10144b50e
L00tefisk <Eivind.93@hotmail.com>
2014-12-04 19:48:43 +0100

    Created superclass for characters

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Character : MonoBehaviour {
+	public int _speed;
+	public float _hp;
+	public float _rotation;
+	public bool _isAlive;
+
+	public abstract void Start ();
+
+	public abstract void Update ();
+
+	public void Damage(float damage) {
+		_hp -= damage;
+		
+		if (_hp <= 0 && _isAlive)
+			Kill();
+	}
+	public abstract void Kill();
+
+	public void Nuke() {
+		Destroy(this.gameObject);
+	}
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,45 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyController : MonoBehaviour {
-	int speed;
-	int hp;
-	int score;
-	private bool isAlive;
+public class EnemyController : Character {
 	private Animator anim;
 	Vector2 velmod;
 	// Use this for initialization
-	void Start () {
-		isAlive = true;
-		speed = 20;
-		hp = 50;
+	public override void Start () {
+		_isAlive = true;
+		_speed = 20;
+		_hp = 50;
 		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	public override void Update () {
 		velmod = Utils.getVelocityModifier(transform.localEulerAngles.z-180);
-		rigidbody2D.AddForce(speed * velmod * 40 * Time.deltaTime);
-		
+		rigidbody2D.AddForce(_speed * velmod * 40 * Time.deltaTime);
+
 		if (rigidbody2D.position.x < -20) {
 			Nuke();
 		}
 	}
-	void Damage (int damage) {
-		hp -= damage;
-		
-		if (hp <= 0 && isAlive)
-			Kill();
-		
-	}
-	void Kill () {
-		isAlive = false;
+	public override void Kill () {
+		_isAlive = false;
 		rigidbody2D.gravityScale = 1;
 		anim.SetTrigger("Die");
 		Invoke ("Nuke", 3);
 		GetComponent<PolygonCollider2D>().enabled = false;
-	}
-	void Nuke() {
-		Destroy(this.gameObject);
 	}
 }
