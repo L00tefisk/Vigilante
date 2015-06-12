f7308aa280323180e7d31d3ef7120f017fa1da07
L00tefisk <Eivind.93@hotmail.com>
2015-06-12 12:36:58 +0200

    Improved XML parser

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,30 +1,38 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class Level {
+public class Level
+{
 	public string map;
 	public List<Wave> Waves;
 	
-	public enum Pattern { Line, Square, Circle }
+	public enum Pattern
+	{
+		Line,
+		Square,
+		Circle
+	}
 	
-	public struct Wave {
+	public struct Wave
+	{
 		public int time;
 		public string enemytype;
 		public int amount;
 		public Pattern pattern;
 	}
 	
-	public Level () {
+	public Level()
+	{
 		Waves = new List<Wave>();
 	}
 	
-	public void addToWaveList(Wave wave) {
+	public void addToWaveList(Wave wave)
+	{
 		this.Waves.Add(wave);
- 	}
+	}
  	
- 	
- 	
-	public void addToWaveList(int time, string enemytype, int amount, Pattern pattern) {
+	public void addToWaveList(int time, string enemytype, int amount, Pattern pattern)
+	{
 		Wave wave = new Wave();
 		wave.time = time;
 		wave.enemytype = enemytype;
@@ -32,13 +40,18 @@ public class Level {
 		wave.pattern = pattern;
 		this.Waves.Add(wave);
 	}
-	public List<Wave> getWaveList() {
+
+	public List<Wave> getWaveList()
+	{
 		return this.Waves;
 	}
-	public void Print() {
+
+	public void Print()
+	{
 		Debug.Log(map);
-		foreach (Wave wave in this.Waves) {
-			Debug.Log("Wave " + (Waves.IndexOf(wave)+1) + 
+		foreach (Wave wave in this.Waves)
+		{
+			Debug.Log("Wave " + (Waves.IndexOf(wave) + 1) + 
 				"\n\tLength: " + wave.time + 
 				"\n\tEnemy: " + wave.enemytype +
 				"\n\tAmount: " + wave.amount +
--- Assets/Scripts/XMLParser.cs
+++ Assets/Scripts/XMLParser.cs
@@ -7,120 +7,102 @@ using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
 
-
 public class XMLParser : MonoBehaviour
 {	
+	private string assetPath;
 	private string projectPath;
 	
-	void Start()
+	void Start ()
 	{ 
-		string projectPath = Application.dataPath + "/Levels/";
+		assetPath = "/Resources/Levels/";
+		projectPath = Application.dataPath + assetPath;
 		
 		if (projectPath != "") {
-			Debug.Log("Running in "+projectPath);
-			Level level = Deserialize("test");
-			Serialize(level, "test2");
-			Debug.Log("Success!");
+			Debug.Log ("Running in " + projectPath);
+			Level level = Deserialize<Level> ("test");
+			Serialize (level, "test2");
+			Debug.Log ("Success!");
 		}
 		//ImportLevels();
 	}
 	
-	public void TypeSerialize(System.Object obj, string filename) 
+	public void Serialize (System.Object obj, string filename)
 	{
-		Type t = obj.GetType();
 		string filePath = projectPath + filename + ".xml";
-		XmlSerializer serializer = new XmlSerializer(t);
+		XmlSerializer serializer = new XmlSerializer (obj.GetType ());
 		
-		TextWriter writer = new StreamWriter(filePath);
+		TextWriter writer = new StreamWriter (filePath);
 		
-		Debug.Log("Serializing " + filePath);
-		serializer.Serialize(writer, obj);
-		writer.Close();
-  }
+		Debug.Log ("Serializing " + filePath);
+		serializer.Serialize (writer, obj);
+		writer.Close ();
+	}
   
-  
-  public Level Deserialize(string filename) //Returns a deserialized object.
+	public T Deserialize<T> (string filename) //Returns a deserialized object.
 	{ 
 		string filePath = projectPath + filename + ".xml";
-		XmlSerializer serializer = new XmlSerializer(typeof(Level));
+		XmlSerializer serializer = new XmlSerializer (typeof(T));
 		
 		// A FileStream is needed to read the XML document.
-		FileStream fileStream = new FileStream(filePath, FileMode.Open);
-		Debug.Log("Deserializing " + filePath);
+		FileStream fileStream = new FileStream (filePath, FileMode.Open);
+		Debug.Log ("Deserializing " + filePath);
 		
-		XmlReader reader = XmlReader.Create(fileStream);
+		XmlReader reader = XmlReader.Create (fileStream);
 		
 		// Declare an object variable of the type to be deserialized.
-		Level i;
+		T i;
 		
 		// Use the Deserialize method to restore the object's state.
-		i = (Level)serializer.Deserialize(reader);
-		fileStream.Close();
+		i = (T)serializer.Deserialize (reader);
+		fileStream.Close ();
 		return i;
-  }
-  public void Serialize(Level level, string filename) 
-  {
-		string filePath = projectPath + filename + ".xml";
-    	XmlSerializer serializer = new XmlSerializer(typeof(Level));
-    	
-		TextWriter writer = new StreamWriter(filePath);
-		
-		Debug.Log("Serializing " + filePath);
-		serializer.Serialize(writer, level);
-		writer.Close();
-  }
-  
-	
-	public void ImportLevels()
+	}
+	/*
+	public void ImportLevels ()
 	{
-		XmlDocument xmlDoc = new XmlDocument();
-		TextAsset text = Resources.Load<TextAsset>("Levels/levels");
-		xmlDoc.LoadXml(text.text);
+		XmlDocument xmlDoc = new XmlDocument ();
+		TextAsset text = Resources.Load<TextAsset> ("Levels/levels");
+		xmlDoc.LoadXml (text.text);
 		
-		List<Level> levelsList = new List<Level>();
+		List<Level> levelsList = new List<Level> ();
 		
-		foreach (XmlNode levelNode in xmlDoc.GetElementsByTagName("level"))//XMLNodeList
-		{
+		foreach (XmlNode levelNode in xmlDoc.GetElementsByTagName("level")) {//XMLNodeList
 			XmlNodeList levelNodeContent = levelNode.ChildNodes;
-			Level level = new Level();
-			foreach (XmlNode item in levelNodeContent) // levels itens nodes.
-			{
-				if  (item.Name == "map") 
-				{
+			Level level = new Level ();
+			foreach (XmlNode item in levelNodeContent) { // levels itens nodes.
+				if (item.Name == "map") {
 					level.map = item.InnerText;
-					Debug.Log("level = " + item.InnerText);
+					Debug.Log ("level = " + item.InnerText);
 				}
 				
-				if  (item.Name == "wave") 
-				{
-					Level.Wave wave = new Level.Wave();
-					foreach (XmlNode waveChild in item.ChildNodes) 
-					{
-						switch (waveChild.Name) 
-						{
+				if (item.Name == "wave") {
+					Level.Wave wave = new Level.Wave ();
+					foreach (XmlNode waveChild in item.ChildNodes) {
+						switch (waveChild.Name) {
 						case "time":
-							int.TryParse(waveChild.InnerText, out wave.time);
+							int.TryParse (waveChild.InnerText, out wave.time);
 							break;
 						case "enemytype":
 							wave.enemytype = waveChild.InnerText;
 							break;
 						case "amount":
-							int.TryParse(waveChild.InnerText, out wave.amount);
+							int.TryParse (waveChild.InnerText, out wave.amount);
 							break;
 						case "pattern":
-							wave.pattern = (Level.Pattern)Enum.Parse(typeof(Level.Pattern), waveChild.InnerText);
+							wave.pattern = (Level.Pattern)Enum.Parse (typeof(Level.Pattern), waveChild.InnerText);
 							break;
 						}
 					}
-					level.addToWaveList(wave);
+					level.addToWaveList (wave);
 				}
 			}
-			levelsList.Add(level);
-			XmlSerializer ser = new XmlSerializer(typeof(Level));
-			TextWriter writer = new StreamWriter("E:/Dokumenter/Development/Unity/Vigilante/Assets/Resources/Levels/test.xml");
-			ser.Serialize(writer, level);
-			writer.Close();
-			level.Print();
+			levelsList.Add (level);
+			XmlSerializer ser = new XmlSerializer (typeof(Level));
+			TextWriter writer = new StreamWriter ("E:/Dokumenter/Development/Unity/Vigilante/Assets/Resources/Levels/test.xml");
+			ser.Serialize (writer, level);
+			writer.Close ();
+			level.Print ();
 		}
 	}
+	*/
 }
